b32cfc4ec0d22272aa58dfac3a2c5291c057563b
saiful-4321 <bd.saif321@gmail.com>
2023-01-05 02:53:02 +0600

    related route added

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gorilla/mux"
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -12,6 +14,13 @@ type Director struct {
 	Position string `json:"position"`
 }
 
-func main() {
+var movies []Movie
 
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/movies", GetMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
+	r.HandleFunc("/movie", CreateMovie).Methods("POST")
+	r.HandleFunc("/movie/{id}", UpdateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", DeleteMovie).Methods("DELETE")
 }
