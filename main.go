5cc711ee73c67ccc2b29efb54acde717f1835219
saiful-4321 <bd.saif321@gmail.com>
2023-01-06 03:47:19 +0600

    method created to get specific movie using movie id

--- main.go
+++ main.go
@@ -63,3 +63,16 @@ func DeleteMovie(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+}
